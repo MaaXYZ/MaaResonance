8478be89709cb522aaa2d2378ff4b0b1c8797000
KevinT3Hu <kevint3hu@outlook.com>
2024-04-04 23:32:15 +0800

    fix: top level await cannot build

--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,45 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { resolve } from 'path'
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: tag => tag.startsWith('mdui-')
-        }
-      }
-    })
-  ],
+    plugins: [
+        vue({
+            template: {
+                compilerOptions: {
+                    isCustomElement: (tag) => tag.startsWith("mdui-"),
+                },
+            },
+        }),
+    ],
 
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
-  clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
-  server: {
-    port: 1420,
-    strictPort: true,
-    watch: {
-      // 3. tell vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
+    // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+    //
+    // 1. prevent vite from obscuring rust errors
+    clearScreen: false,
+    // 2. tauri expects a fixed port, fail if that port is not available
+    server: {
+        port: 1420,
+        strictPort: true,
+        watch: {
+            // 3. tell vite to ignore watching `src-tauri`
+            ignored: ["**/src-tauri/**"],
+        },
     },
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
+    resolve: {
+        alias: {
+            "@": resolve(__dirname, "src"),
+        },
     },
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        miniwindow: resolve(__dirname, 'mini.html'),
-        settingswindow: resolve(__dirname, 'settings.html')
-      }
-    }
-  }
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                miniwindow: resolve(__dirname, "mini.html"),
+                settingswindow: resolve(__dirname, "settings.html"),
+            },
+        },
+        target: "esnext",
+    },
 }));
